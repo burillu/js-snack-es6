bb1163c6f3f2d58d3968b5e9e03fbf2829ec86f0
burillu <burillu90@hotmail.it>
2023-10-24 16:35:28 +0200

    created dir snacks bonus

---
+++ snaks-aggiuntivi/js/script.js
@@ -0,0 +1,35 @@
+/* 
+A partire da un array di stringhe, crea un secondo array formattando le
+stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+*/
+
+
+
+/*
+**********************************************************************************************************************
+
+- Snak 3
+
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+{ nome: ‘leone’, famiglia: ‘felidi’, classe: ‘mammiferi’ },
+{ nome: ‘cane’, famiglia: ‘canidi’, classe: ‘mammiferi’ },
+{ nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ },
+]
+Crea un nuovo array con la lista dei mammiferi. */
+
+
+/*
+**********************************************************************************************************************
+
+- Snak 4
+
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il
+nome e cognome e l’indicazione se può guidare, in base all’età. 
+
+*/
